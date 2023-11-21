071a2fb9049c79cf374c34894dd24c894d8a823b
Vương Phương Thảo <125387948+pthao12@users.noreply.github.com>
2023-11-21 10:15:03 +0700

    add Wordament

---
+++ src/main/java/Core/Wordament/Wordament.java
@@ -0,0 +1,18 @@
+package main.java.Core.Wordament; 
+public class Wordament {
+    private String word;
+    private String meaning;
+
+    public Wordament(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+}
